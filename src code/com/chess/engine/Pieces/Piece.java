9a9f4135c00516a368c51343ef0bb2ed0a2d0bfa
ponma304 <ponma304@student.otago.ac.nz>
2023-06-29 12:17:35 +1200

    Vid #2 completed and thinking of minnoring in SE

---
+++ src code/com/chess/engine/Pieces/Piece.java
@@ -0,0 +1,8 @@
+package com.chess.engine.Pieces;
+
+public class Piece {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
---
+++ src code/com/chess/engine/board/Tile.java
@@ -0,0 +1,87 @@
+package com.chess.engine.board;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.chess.engine.Pieces.Piece;
+
+public abstract class Tile {
+    protected final int tileCoords;
+    private static final Map<Integer, EmptyTile> EmptyTileMap = createAllPossEmptyTile();
+
+    private Tile(int tileCoords) {
+        this.tileCoords = tileCoords;
+    }
+
+    // Returns if a tile is Occupied
+    public abstract boolean isTileOccupied();
+
+    // Returns the piece on tile
+    public abstract Piece getPiece();
+
+    public static Tile createTle(final int titleCoordinate, final Piece piece) {
+        if (piece != null) {
+
+            return new OccupiedTile(titleCoordinate, piece);
+
+        } else {
+
+            return EmptyTileMap.get(titleCoordinate);
+
+        }
+    }
+
+    private static Map<Integer, EmptyTile> createAllPossEmptyTile() {
+
+        final Map<Integer, EmptyTile> EmptyTileMap = new HashMap<>();
+
+        for (int index = 0; index < 64; index++) {
+            EmptyTileMap.put(index, new EmptyTile(index));
+        }
+        // In the og design the method would return a ImuntableMap.copyOf(EmptyTileMap)
+        // version of EmptyTileMap
+        // which would prevent EmptyTileMap from being edited when referenced. I couldnt
+        // be dammed to downlad
+        // the libary and figure out how to use the libary so ill just wait for lab dem
+        // help.
+        return EmptyTileMap;
+    }
+
+    public static final class EmptyTile extends Tile {
+
+        EmptyTile(final int tileCoords) {
+            super(tileCoords);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends Tile {
+
+        private final Piece pieceOnTile;
+
+        OccupiedTile(int tileCoords, Piece pieceOnTile) {
+            super(tileCoords);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+
+    }
+}
